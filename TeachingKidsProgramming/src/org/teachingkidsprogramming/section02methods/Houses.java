15bfdffbc802d5e3ab4873fce0c62752bbd036cf
Ngmangibin <ngmangibin@gmail.com>
2016-01-21 12:02:33 -0600

    Full Skyscraper

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/Houses.java
@@ -8,10 +8,23 @@ public class Houses
   {
     Tortoise.show();
     //   Make the tortoise move as fast as possible --#11
+    Tortoise.setSpeed(10);
     //   Have the tortoise start at 200 pixels in on the X axis --#14
+    Tortoise.setX(200);
     //   The current height is 40 --#1.2
     int currentHeight = 40;
     //   drawHouse (recipe below) --#9.1
+    drawHouse(currentHeight);
+    //   ------------- End of drawHouse recipe --#9.3
+    //   drawHouse with height 120 (recipe below) --#10
+    drawHouse(120);
+    //   drawHouse with height 90 (recipe below) --#12
+    drawHouse(90);
+    //   drawHouse with height 20 (recipe below) --#13
+    drawHouse(20);
+  }
+  private static void drawHouse(int currentHeight)
+  {
     //   ------------- Recipe for drawHouse --#9.2
     //       Change the pen color of the line the tortoise draws to lightGray --#15
     //       Move the tortoise the height of a house --#1.1
@@ -29,9 +42,6 @@ public class Houses
     //       Move the tortoise 20 pixels --#7
     Tortoise.move(20);
     //       Turn the tortoise 90 degrees to the left --#8
-    //   ------------- End of drawHouse recipe --#9.3
-    //   drawHouse with height 120 (recipe below) --#10
-    //   drawHouse with height 90 (recipe below) --#12
-    //   drawHouse with height 20 (recipe below) --#13
+    Tortoise.turn(-90);
   }
 }
