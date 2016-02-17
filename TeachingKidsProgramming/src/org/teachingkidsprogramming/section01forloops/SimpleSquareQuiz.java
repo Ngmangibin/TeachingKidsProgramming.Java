3c4d603aef8fe3cc78f863cd81e5c860ae4ce123
Ngmangibin <ngmangibin@gmail.com>
2016-02-17 12:14:39 -0600

    Revert "Quiz 1"
    This reverts commit d03c909f034190e6f3da4bab33e630fd28e12c81.

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquareQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquareQuiz.java
@@ -1,7 +1,5 @@
 package org.teachingkidsprogramming.section01forloops;
 
-import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SimpleSquareQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SquareQuiz;
 
@@ -9,23 +7,19 @@ public class SimpleSquareQuiz implements SquareQuiz
 {
   public void question1()
   {
-    //  Move the tortoise 110 pixels\
-    Tortoise.move(110);
+    //  Move the tortoise 110 pixels
   }
   public void question2()
   {
     //  Turn the tortoise 1/5 of 360 degrees to the right
-    Tortoise.turn(72);
   }
   public void question3()
   {
     //  Change the pen color the tortoise draws to yellow
-    Tortoise.setPenColor(PenColors.Yellows.Yellow);
   }
   public void question4()
   {
     //  Change the width of the line the tortoise draws to 5 pixels
-    Tortoise.setPenWidth(5);
   }
   public static void main(String[] args)
   {
