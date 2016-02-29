ef9381cbc194e4216a85225e3dda4471aff74ada
Ngmangibin <ngmangibin@gmail.com>
2016-02-29 12:33:06 -0600

    Adlib1

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section06modelviewcontroller/AdLibs.java
@@ -1,16 +1,26 @@
 package org.teachingkidsprogramming.section06modelviewcontroller;
 
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
 public class AdLibs
 {
   public static void main(String[] args)
   {
     //  Ask the user to enter an adverb, save it as currentAdverb --#2
+    String currentAdverb = MessageBox.askForTextInput("Adverb?");
     //  Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
+    String currentEdVerb = MessageBox.askForTextInput("-ed Verb?");
     //  Ask the user to enter a body part, save it as currentBodyPart --#6
-    //  Set the value of the currentStory to the word "Today " --#1.2
+    String currentBodyPart = MessageBox.askForTextInput("Body part?");
+    //  Set the value of the currentStory to the word "Today" --#1.2
+    String currentStory = "Today ";
     //  Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
+    currentStory = currentStory + "I woke " + currentAdverb + ". ";
     //  Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
+    currentStory = currentStory + "Then I " + currentEdVerb + " ";
     //  Add the words "my " + currentBodyPart + ". " to the current story --#7
+    currentStory = currentStory + "my " + currentBodyPart + ". ";
     //  Show the currentStory in a message box as a message --#1.1
+    MessageBox.showMessage(currentStory);
   }
 }
